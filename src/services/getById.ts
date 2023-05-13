30cb72f915a26a557aa40b175835cf0719347e0f
Andrés Weitzel <andres96energy@hotmail.com>
2023-05-12 22:35:52 -0300

    Added get products by your id without date

--- src/services/getById.ts
+++ src/services/getById.ts
@@ -1,11 +1,13 @@
 //Externals
-//const { Sequelize } = require("sequelize");
-import {Sequelize} from "sequelize";
+import { Sequelize } from "sequelize";
 //Models
 import { Product } from "src/models/Product";
 import { ProductSequelize } from "src/models/ProductSequelize";
+//Enums
+import { statusName } from "src/enums/connection/statusName";
 //Const/Vars
-let product;
+let product: any;
+let msg: string
 
 /**
  * @description Get a product with all the attributes from the database according to the id passed as a parameter
@@ -13,83 +15,90 @@ let product;
  * @returns a Product according to his id
  * @example
  */
-export const getById = async function (id:number) {
+export const getById = async function (id: number) {
   try {
     product = null;
 
     if (Product != null) {
-       await ProductSequelize.findByPk(id
-      , {
-        attributes: {
-          include: [
-            [
-              Sequelize.fn(
-                "DATE_FORMAT",
-                Sequelize.col("creation_date"),
-                "%Y-%m-%d %H:%i:%s"
-              ),
-              "creation_date",
+      await ProductSequelize.findByPk(id
+        , {
+          attributes: {
+            include: [
+              [
+                Sequelize.fn(
+                  "DATE_FORMAT",
+                  Sequelize.col("creation_date"),
+                  "%Y-%m-%d %H:%i:%s"
+                ),
+                "creation_date",
+              ],
+              [
+                Sequelize.fn(
+                  "DATE_FORMAT",
+                  Sequelize.col("update_date"),
+                  "%Y-%m-%d %H:%i:%s"
+                ),
+                "update_date",
+              ],
             ],
-            [
-              Sequelize.fn(
-                "DATE_FORMAT",
-                Sequelize.col("update_date"),
-                "%Y-%m-%d %H:%i:%s"
-              ),
-              "update_date",
-            ],
-          ],
-        },
-      }
+          },
+        }
       )
         .then((findProduct) => {
           product = findProduct;
         })
         .catch((error) => {
-          console.log(error);
+          msg = `Error in get product according to your id. Caused by ${error}`;
+          console.error(`${msg}. Stack error type : ${error.stack}`);
+          product = statusName.CONNECTION_REFUSED;
         });
     } else {
-      product = "ECONNREFUSED";
+      product = statusName.CONNECTION_REFUSED;
     }
-    
-  return product;
+
+    return product;
   } catch (error) {
-    console.log(error);
-    product = "ERROR";
+    msg = `Error in getById function. Caused by ${error}`;
+    console.error(`${msg}. Stack error type : ${error.stack}`);
+    product = statusName.CONNECTION_ERROR;
   }
 };
 
-// /**
-//  * @description gets a Product with id, nickname, email, identification and country attributes whose id matches the one passed as a parameter
-//  * @param {Number} id Number type
-//  * @returns a Product according to his id
-//  * @example
-//  * {"id":2,"nickname":"JAVIER GONZALEZ","email":"javiBoquita@gmail.com","identification_type":"DNI","identification_number":"2672268765","country_id":"AR"}
-//  */
-// const getByIdLimit = async function (id) {
-//   try {
-//     Product = null;
-//     checkDbConn = await checkDbAuthentication();
 
-//     if (checkDbConn && Product != null) {
-//       await Product.findByPk(id, {
-//         attributes: {
-//           exclude: ["first_name", "last_name", "creation_date", "update_date"],
-//         },
-//       })
-//         .then((findProduct) => {
-//           Product = findProduct;
-//         })
-//         .catch((error) => {
-//           console.log(error);
-//         });
-//     } else {
-//       Product = "ECONNREFUSED";
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     Product = "ERROR";
-//   }
-//   console.log(Product);
-//   return Product;
-// };
+/**
+ * @description Get a product with all the attributes without date from the database according to the id passed as a parameter
+ * @param {Number} id Number type
+ * @returns a Product according to his id
+ * @example
+ */
+export const getByIdWithoutDate = async function (id: number) {
+  try {
+    product = null;
+
+    if (Product != null) {
+      await ProductSequelize.findByPk(id
+        , {
+          attributes: {
+            exclude: ["creation_date", "update_date"],
+          },
+        }
+      )
+        .then((findProduct) => {
+          product = findProduct;
+        })
+        .catch((error) => {
+          msg = `Error in get product according to your id. Caused by ${error}`;
+          console.error(`${msg}. Stack error type : ${error.stack}`);
+          product = statusName.CONNECTION_REFUSED;
+        });
+    } else {
+      product = statusName.CONNECTION_REFUSED;
+    }
+
+    return product;
+  } catch (error) {
+    msg = `Error in getByIdWithoutDate function. Caused by ${error}`;
+    console.error(`${msg}. Stack error type : ${error.stack}`);
+    product = statusName.CONNECTION_ERROR;
+  }
+};
